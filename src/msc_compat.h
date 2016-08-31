8844ca55fb1a5bcccbd725a466becb1e15120c23
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-08-31 12:09:04 +0200

    Define PACK() macro to allow packed structs using both gcc & msvc

---
+++ src/msc_compat.h
@@ -0,0 +1,28 @@
+/*
+* msc_compat.h
+* Provides helper code which allows idevicerestore to compile using Visual C++
+*
+* Copyright (c) 2016 Quamotion bvba. All Rights Reserved.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#pragma once
+
+#ifndef _MSC_VER
+#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#else
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+#endif
